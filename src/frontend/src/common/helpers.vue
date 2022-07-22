598991d7df8285b689becbe75b600e530fc3a4c9
SychugovKirill <9090000640@mail.ru>
2022-07-22 18:53:08 +0500

    create helpers.vue

---
+++ src/frontend/src/common/helpers.vue
@@ -0,0 +1,36 @@
+<script>
+const setFillingType = (item) => {
+  switch (item) {
+    case "Грибы":
+      return "mushrooms";
+    case "Чеддер":
+      return "cheddar";
+    case "Салями":
+      return "salami";
+    case "Ветчина":
+      return "ham";
+    case "Ананас":
+      return "ananas";
+    case "Бекон":
+      return "bacon";
+    case "Лук":
+      return "onion";
+    case "Чили":
+      return "chile";
+    case "Халапеньо":
+      return "jalapeno";
+    case "Маслины":
+      return "olives";
+    case "Томаты":
+      return "tomatoes";
+    case "Лосось":
+      return "salmon";
+    case "Моцарелла":
+      return "mozzarella";
+    case "Пармезан":
+      return "parmesan";
+    case "Блю чиз":
+      return "blue_cheese";
+  }
+};
+</script>
